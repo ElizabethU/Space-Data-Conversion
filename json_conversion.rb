602158803d815ea2fc84623c31c2676254760805
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-02 12:34:05 -0700

    makes a wonderful horrible hash

--- json_conversion.rb
+++ json_conversion.rb
@@ -4,7 +4,18 @@ def make_arrays(datafile)
   input.each do |line|
     output << line.split
   end
-  puts output
+  output
 end
 
-make_arrays('planetdata.txt')
+def make_hash(datafile)
+  hash = {}
+  make_arrays('planetdata.txt').each do |line|
+    unless hash[line[0]]
+      hash[line[0]] = {}
+    end
+    hash[line[0]][line[1]] = {x: line[2], y: line[3], z: line[4]}
+  end
+  puts hash
+end
+
+make_hash('planetdata.txt')
