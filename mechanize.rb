9ca30523657f69f0534dbec0ae7d27510ad64404
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-04 14:57:06 -0700

    Added form data

--- mechanize.rb
+++ mechanize.rb
@@ -8,4 +8,18 @@ class Datagetter
     @table = (@page.root / :pre).text.scan(/\s*(.*?)\s*(\d{4})  (\d{3}) - (\d{4})  (\d{3})/)
   end
 
+  def submit_form
+    i = 0
+    @table.each do |body|
+      outer_space_form = @page.form 
+      outer_space_form.field_with(name: 'object').options[i].select
+      outer_space_form.start_year = body[1]
+      outer_space_form.start_day = body[2]
+      outer_space_form.stop_year = body[3]
+      outer_space_form.stop_day = body[4]
+      outer_space_form.radiobuttons_with(name: 'activity')[3].check
+      i += 1
+      #you need to submit the form now, and call the scraping and json methods in here
+    end
+  end
 end
