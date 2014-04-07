2834e0c8f76772cfd7556c44b21f1b98d0e7127f
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-07 11:28:35 -0700

    more planet scraping

--- planet_scraper.rb
+++ planet_scraper.rb
@@ -4,10 +4,21 @@ class planet_data_getter
   def initialize
     @agent = Mechanize.new
     @page = @agent.get('http://omniweb.gsfc.nasa.gov/coho/helios/planet.html')
+    @planet_array = ["Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Earth", "Mercury"]
     #@table = (@page.root / :pre).text.scan(/\s*(.*?)\s*(\d{4})  (\d{3}) - (\d{4})  (\d{3})/)
   end
 
   def submit_form
-    i = 0
+    for n in range(0..8)
+      planet_form = @page.form
+      planet_form.field_with(name: 'planet').options[n].select
+      planet_form.radiobuttons_with(name: 'activity')[0].check
+      planet_form.start_year = 1959
+      planet_form.start_day  = 1
+      planet_form.stop_year  = 2019
+      planet_form.stop_day   = 365
+      @new_page = @agent.submit(planet_form)
+      data = (@new_page.root / :pre).text.scan(/(\d{4}) \s*?(\d{1,3})\s*?(\+|-?[0-9]+\.[0-9]+)\s*?(\+|-?[0-9]+\.[0-9]+)\s*?(\+|-?[0-9]+\.[0-9]+)\s*?(\+|-?[0-9]+\.[0-9]+)\s*?(\+|-?[0-9]+\.[0-9]+)\s*?(\+|-?[0-9]+\.[0-9]+)/)
+    end
   end
 end
